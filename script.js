17c5679990426840c88d0851b9e83b92767b5a18
Alan Amalemba <alanlsamalemba@gmail.com>
2024-09-22 18:57:15 +0300

    feat: done with dektop view

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+const currentYear = document.querySelector("#current-year");
+
+currentYear.textContent = new Date().getFullYear();
